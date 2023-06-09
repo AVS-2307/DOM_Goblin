734f0702294729aee1fb689dbf822a6538f46434
Andrey Shestakov <andrey230781.an@gmail.com>
2023-06-09 19:40:48 +0300

    webpack files reconfig

--- webpack.common.js
+++ webpack.common.js
@@ -3,54 +3,48 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	target: 'web',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
           },
         ],
       },
-			{//это должно быт здесь, тк есть встроенный лоадер, способный обрабатывать такие файлы
-				test: /\.(?:ico|png|jpg|jpeg|gif)$/i,
-					
-				type:'asset/resource',
-					
-      },
-			{
-				test: /\.css$/,
-				use: [
+      {
+        test: /\.css$/,
+        use: [
           MiniCssExtractPlugin.loader, 'css-loader',
         ],
       },
-			{
-				test: /\.svg$/,
-				type: 'asset/resource',
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
-	},
-	plugins: [
+  },
+  plugins: [
     new HtmlWebPackPlugin({
-			template: './src/index.html',
-			filename: './index.html',
-		}),
+      template: './src/index.html',
+      filename: './index.html',
+    }),
     new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css',
-		}),
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
 };
